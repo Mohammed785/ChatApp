cc924e54eb75cd84f21198a6562eb8ee1449112d
Mohammed <mohammednady128@gmail.com>
2022-01-23 11:44:13 +0200

    tweaks and updates to client

--- public/js/client.js
+++ public/js/client.js
@@ -4,44 +4,68 @@ const searchForm = document.getElementById("search-form");
 const chatArea = document.querySelector(".chat-area-main");
 const conversionArea = document.getElementById("friends-chats");
 const logoutBtn = document.getElementById("logout");
-var friendsChats = null;
-var currentChat = null;
-var userInfo = null;
+let friendsChats = null;
+let currentChat = null;
+let userInfo = null;
 
+const chatEvents = ()=>{
+    friendsChats = document.querySelectorAll(".chat");
+    deleteChatsA = document.querySelectorAll(".del-x")
+    friendsChats.forEach((chat) => {
+        chat.addEventListener("click", function (e) {
+            loadChatMsgs($(this).attr("id"));
+            currentChat = $(this).attr("id");
+            chat.children[1].children[1].innerHTML='';
+        });
+    });
+    
+    deleteChatsA.forEach((a) => {
+        a.addEventListener("click", function (e) {
+            const chatId= $(this.parentElement).attr("id");
+            deleteChatApi(chatId).then(conversionArea.removeChild(this.parentElement));
+        });
+    });
+}
+//socket
 socket.on("check", () => {
     getUserInfo().then((data) => {
-        getUserChats().then((chats) => {
-            handleUserChats(chats).then(() => {
-                friendsChats = document.querySelectorAll(".chat");
-                friendsChats.forEach((chat) => {
-                    chat.addEventListener("click", function (e) {
-                        loadChatMsgs($(this).attr("id"));
-                        currentChat = $(this).attr("id");
-                    });
-                });
-            });
-        });
+        handleChatLogic()
     });
 });
 
+socket.on('newChat',(data)=>{
+    console.log(data);
+    if(data.members[1]._id==userInfo.userId){
+        console.log("added");
+        conversionArea.innerHTML+= addUserChat(data)
+        chatEvents()
+    }
+})
+//messages
 socket.on("message", (data) => {
     const msg = createMessage(data);
-    chatArea.innerHTML += msg;
+    if (data.chat === currentChat) {
+        chatArea.innerHTML += msg;
+    } else {
+        const chat = document.getElementById(data.chat);
+        if(chat){
+            chat.children[1].children[1].innerHTML = `<span class="msg-message">${data.content}</span>`;
+        }
+        else{
+            handleChatLogic()
+        }
+    }
     smoothScroll();
 });
 
 const createMessage = (data) => {
-    let style =
-        data?.sender?.email !== userInfo.email ? "chat-msg" : "chat-msg owner";
+    let style = data?.sender?.email !== userInfo.email ? "chat-msg" : "chat-msg owner";
     if (data?.userEmail) {
-        style =
-            data.userEmail !== userInfo.email ? "chat-msg" : "chat-msg owner";
+        style = data.userEmail !== userInfo.email ? "chat-msg" : "chat-msg owner";
     }
     return `<div class="${style}" data-id=${data.id}>
      <div class="chat-msg-profile">
-      <div class="chat-msg-date">Message sent ${new Date(
-          data.createdAt
-      ).toLocaleString()}</div>
+      <div class="chat-msg-date">Message sent ${new Date(data.createdAt).toLocaleString()}</div>
      </div>
      <div class="chat-msg-content">
       <div class="chat-msg-text">${data.content}</div>   
@@ -70,60 +94,6 @@ form.addEventListener("submit", (e) => {
     });
 });
 
-const getUserInfo = () => {
-    return $.ajax({
-        method: "GET",
-        url: "/api/v1/user/info",
-        success: (data) => {
-            userInfo = data;
-        },
-        error: (error) => {
-            console.error(error);
-            window.location.href = "/auth.html";
-        },
-    });
-};
-const getUserChats = () => {
-    return $.ajax({
-        method: "GET",
-        url: "/api/v1/chat/find/user-chats",
-        error: (error) => {
-            console.error(error);
-        },
-    });
-};
-
-const handleUserChats = (chats) => {
-    return new Promise((resolve, reject) => {
-        if (chats.length === 0) {
-            conversionArea.innerHTML = `<div class='no-chats'><p>No Chats Found Add One</p></div>`;
-            return reject();
-        }
-        let data = chats.chats
-            .map((chat) => {
-                return `<div class="msg chat" id="${chat._id}">
-              <!-- <img class="msg-profile" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%281%29.png"/> -->
- 
-    <div class="msg-detail">
-     <div class="msg-username">${
-         chat.members.find((member, index) => {
-             if (member._id !== userInfo.userId) {
-                 return true;
-             }
-         }).username
-     }</div>
-     <div class="msg-content">
-      <!----<span class="msg-message">What time was our meet</span>
-      <span class="msg-date">20m</span>---->
-     </div>
-    </div>
-   </div>`;
-            })
-            .join("");
-        conversionArea.innerHTML = data;
-        return resolve();
-    });
-};
 const loadChatMsgs = (chatId) => {
     $.ajax({
         method: "GET",
@@ -138,15 +108,79 @@ const loadChatMsgs = (chatId) => {
 };
 
 const handelChatMsgs = (msgs) => {
-    const data = msgs.messages
-        .map((chat) => {
-            return createMessage(chat);
-        })
-        .join("");
+    const data = msgs.messages.map((chat) => { return createMessage(chat) }).join("");
     chatArea.innerHTML = data;
     chatArea.scrollTop = chatArea.scrollHeight;
     smoothScroll();
 };
+//user info
+const getUserInfo = () => {
+    return $.ajax({
+        method: "GET",
+        url: "/api/v1/user/info",
+        success: (data) => {
+            userInfo = data;
+        },
+        error: (error) => {
+            console.error(error);
+            window.location.href = "/auth.html";
+        },
+    });
+};
+//chats
+const getUserChats = () => {
+    return $.ajax({
+        method: "GET",
+        url: "/api/v1/chat/find/user-chats",
+        error: (error) => {
+            console.error(error);
+        },
+    });
+};
+
+const addUserChat = (chat) => {
+    return `<div class="msg chat" id="${chat._id}">
+            <a href="#" class="del-x">X</a>
+            <div class="msg-detail">
+            <div class="msg-username">${
+                chat.members.find((member, index) => {
+                    if (member._id !== userInfo.userId) {
+                        return true;
+                    }
+                }).username}</div>
+            <div class="msg-content">
+            </div>
+            </div>
+        </div>`;
+};
+
+const deleteChatApi = (id) =>{
+    return $.ajax({
+        method: "DELETE",
+        url: `/api/v1/chat/${id}/delete`,
+        error: (error) => {
+            console.error(error);
+        },
+    });
+}
+const handleUserChats = (chats) => {
+    return new Promise((resolve, reject) => {
+        if (chats.length === 0) {
+            conversionArea.innerHTML = `<div class='no-chats'><p>No Chats Found Add One</p></div>`;
+            return reject();
+        }
+        const data = chats.chats.map((chat) => {return addUserChat(chat)}).join("");
+        conversionArea.innerHTML = data;
+        return resolve();
+    });
+};
+const handleChatLogic = ()=>{
+    getUserChats().then((chats) => {
+        handleUserChats(chats).then(() => {
+            chatEvents();
+        });
+    });
+}
 
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -159,13 +193,14 @@ searchForm.addEventListener("submit", (e) => {
             friendEmail: userEmail,
         }),
         success: (data) => {
-            console.log(data);
+            handleChatLogic()
         },
         error: (error) => {
             console.error(error);
         },
     });
 });
+
 logoutBtn.addEventListener("click", (e) => {
     $.ajax({
         method: "POST",
